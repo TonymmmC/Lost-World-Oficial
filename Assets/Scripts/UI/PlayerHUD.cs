4d332d874f9c3a89e1f114ba8ef82454372942c8
TonymmmC <tonycolores8@gmail.com>
2026-04-23 15:20:24 -0400

    agrego barra de stamina UI y corrijo barra de vida enemigo

--- Assets/Scripts/UI/PlayerHUD.cs
+++ Assets/Scripts/UI/PlayerHUD.cs
@@ -3,18 +3,24 @@ using UnityEngine.UI;
 
 public class PlayerHUD : MonoBehaviour
 {
-    [SerializeField] private RectTransform fillBar;
+    [SerializeField] private Image fillBar;
     [SerializeField] private Health playerHealth;
 
     private void Start()
     {
+        if (playerHealth == null) { Debug.LogError($"{name}: falta referencia a Health"); return; }
         playerHealth.OnChanged += UpdateBar;
         UpdateBar(playerHealth.Current, playerHealth.Max);
     }
 
+    private void OnDestroy()
+    {
+        if (playerHealth != null)
+            playerHealth.OnChanged -= UpdateBar;
+    }
+
     private void UpdateBar(int current, int max)
     {
-        float pct = (float)current / max;
-        fillBar.localScale = new Vector3(pct, 1f, 1f);
+        fillBar.fillAmount = (float)current / max;
     }
 }
